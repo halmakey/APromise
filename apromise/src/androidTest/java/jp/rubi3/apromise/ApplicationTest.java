5ee916fac9397ca3c669ec505b60748fcd32223b
halmakey <halmakey@cubicplus.net>
2016-02-09 15:31:35 +0900

    fix test

--- apromise/src/androidTest/java/jp/rubi3/apromise/ApplicationTest.java
+++ apromise/src/androidTest/java/jp/rubi3/apromise/ApplicationTest.java
@@ -3,6 +3,8 @@ package jp.rubi3.apromise;
 import android.app.Application;
 import android.os.Handler;
 import android.os.Looper;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.test.ApplicationTestCase;
 
 import java.util.concurrent.CountDownLatch;
@@ -15,61 +17,7 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         super(Application.class);
     }
 
-    public void testThen() throws Throwable {
-        final StringBuilder builder = new StringBuilder();
-        final CountDownLatch latch = new CountDownLatch(1);
-
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-            @Override
-            public void run() {
-                Promise.resolved("ABC").then(new Callback<String>() {
-                    @Override
-                    public void callback(String result) {
-                        builder.append(result);
-                    }
-                }).then(new Filter<String, Integer>() {
-                    @Override
-                    public Integer filter(String result) {
-                        return result.length();
-                    }
-                }).then(new Pipe<Integer, StringBuilder>() {
-                    @Override
-                    public Promise<StringBuilder> pipe(Integer result) {
-                        builder.append(result);
-                        return Promise.resolved(builder);
-                    }
-                }).then(new Callback<StringBuilder>() {
-                    @Override
-                    public void callback(StringBuilder result) {
-                        result.append(result.toString());
-                    }
-                }).then(new Callback<StringBuilder>() {
-                    @Override
-                    public void callback(StringBuilder result) {
-                        result.append("1");
-                    }
-                }).catche(new Callback<Exception>() {
-                    @Override
-                    public void callback(Exception result) {
-                        builder.append("X");
-                    }
-                }).all(new Callback<Object>() {
-                    @Override
-                    public void callback(Object result) {
-                        latch.countDown();
-                    }
-                });
-            }
-        });
-
-
-        latch.await();
-
-        assertEquals("ABC3ABC31", builder.toString());
-
-    }
-
-    public void testSuccessCallback() throws Exception {
+    public void testCallback() throws Exception {
         final StringBuilder builder = new StringBuilder();
         final CountDownLatch latch = new CountDownLatch(1);
 
@@ -77,80 +25,31 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
             @Override
             public void run() {
                 Promise.resolved(
-                        builder.append('1')
+                        builder.append("A")
                 ).then(new Callback<StringBuilder>() {
                     @Override
-                    public void callback(StringBuilder result) {
-                        builder.append('2');
-                    }
-                }).then(null, new Callback<Exception>() {
-                    @Override
-                    public void callback(Exception result) throws Exception {
-                        builder.append(result.getClass().getSimpleName());
+                    public void callback(StringBuilder result) throws Exception {
+                        result.append("B");
+                        throw new Exception("C"); // throw on then callback
                     }
                 }).then(new Callback<StringBuilder>() {
                     @Override
-                    public void callback(StringBuilder result) {
-                        builder.append('3');
-                        latch.countDown();
-                    }
-                });
-            }
-        });
-        latch.await();
-
-        assertEquals("123", builder.toString());
-    }
-
-    public void testRejected() throws Throwable {
-        final StringBuilder builder = new StringBuilder();
-        final CountDownLatch latch = new CountDownLatch(1);
-
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-            @Override
-            public void run() {
-                Promise.rejected(new Exception("Hello!")).then(new Callback() {
-                    @Override
-                    public void callback(Object result) {
-                        builder.append("X");
-                    }
-                }).catche(new Callback<Throwable>() {
-                    @Override
-                    public void callback(Throwable result) {
-                        builder.append(result.getMessage());
-                        result.printStackTrace();
-                    }
-                }).all(new Callback<Object>() {
-                    @Override
-                    public void callback(Object result) {
-                        latch.countDown();
-                    }
-                });
-            }
-        });
-
-
-        latch.await();
-        assertEquals("Hello!", builder.toString());
-
-    }
-
-    public void testThrow() throws Throwable {
-        final StringBuilder builder = new StringBuilder();
-        final CountDownLatch latch = new CountDownLatch(1);
-
-        new Handler(Looper.getMainLooper()).post(new Runnable() {
-            @Override
-            public void run() {
-                Promise.resolved(null).then(new Callback<Object>() {
-                    @Override
-                    public void callback(Object result) {
-                        throw new RuntimeException("RuntimeException!");
+                    public void callback(StringBuilder result) throws Exception {
+                        result.append("X"); // through
                     }
                 }).catche(new Callback<Exception>() {
                     @Override
-                    public void callback(Exception result) {
+                    public void callback(Exception result) throws Exception {
                         builder.append(result.getMessage());
+                    }
+                }).all(new Callback<Object>() {
+                    @Override
+                    public void callback(Object result) throws Exception {
+                        if (result instanceof Exception) {
+                            builder.append("D"); // through result object
+                        } else {
+                            builder.append("X");
+                        }
                         latch.countDown();
                     }
                 });
@@ -158,7 +57,197 @@ public class ApplicationTest extends ApplicationTestCase<Application> {
         });
 
         latch.await();
-        assertEquals("RuntimeException!", builder.toString());
+        assertEquals("ABCD", builder.toString());
     }
 
+    public void testFilter() throws Exception {
+        final StringBuilder builder = new StringBuilder();
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        new Handler(Looper.getMainLooper()).post(new Runnable() {
+            @Override
+            public void run() {
+                Promise.resolved(
+                        builder.append("A")
+                ).then(new Filter<StringBuilder, String>() {
+                    @Override
+                    public String filter(StringBuilder result) throws Exception {
+                        builder.append("B");
+                        return "C";
+                    }
+                }).then(new Filter<String, Exception>() {
+                    @Override
+                    public Exception filter(String result) throws Exception {
+                        builder.append(result);
+                        return new Exception("D"); // filter returns Exception to reject
+                    }
+                }).then(new Filter<Exception, String>() {
+                    @Nullable
+                    @Override
+                    public String filter(Exception result) throws Exception {
+                        return "X"; // through
+                    }
+                }).catche(new Filter<Exception, String>() {
+                    @Override
+                    public String filter(Exception result) throws Exception {
+                        builder.append(result.getMessage());
+                        return "E";
+                    }
+                }).all(new Filter<Object, String>() {
+                    @Override
+                    public String filter(Object result) throws Exception {
+                        builder.append(result);
+                        return "F";
+                    }
+                }).all(new Filter<Object, Object>() {
+                    @Override
+                    public Object filter(Object result) throws Exception {
+                        builder.append(result);
+                        latch.countDown();
+                        return null;
+                    }
+                });
+            }
+        });
+
+        latch.await();
+        assertEquals("ABCDEF", builder.toString());
+    }
+
+    public void testPipe() throws Exception {
+        final StringBuilder builder = new StringBuilder();
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Promise.resolved(
+                        "A"
+                ).then(new Pipe<String, String>() {
+                    @NonNull
+                    @Override
+                    public Promise<String> pipe(String result) throws Exception {
+                        builder.append(result);
+                        return Promise.resolved("B");
+                    }
+                }).then(new Pipe<String, String>() {
+                    @NonNull
+                    @Override
+                    public Promise<String> pipe(String result) throws Exception {
+                        builder.append(result);
+                        throw new Exception("C");
+                    }
+                }).then(new Pipe<String, Character>() {
+                    @NonNull
+                    @Override
+                    public Promise<Character> pipe(String result) throws Exception {
+                        builder.append("X");
+                        return null; // cause nullpo
+                    }
+                }).catche(new Pipe<Exception, Character>() {
+                    @NonNull
+                    @Override
+                    public Promise<Character> pipe(Exception result) throws Exception {
+                        builder.append(result.getMessage());
+                        return Promise.rejected(new Exception("D"));
+                    }
+                }).catche(new Pipe<Exception, Character>() {
+                    @NonNull
+                    @Override
+                    public Promise<Character> pipe(Exception result) throws Exception {
+                        builder.append(result.getMessage());
+                        return Promise.resolved('E');
+                    }
+                }).all(new Pipe<Object, CountDownLatch>() {
+                    @NonNull
+                    @Override
+                    public Promise<CountDownLatch> pipe(Object result) throws Exception {
+                        builder.append(result);
+                        latch.countDown();
+                        return Promise.resolved(latch);
+                    }
+                });
+            }
+        });
+
+        latch.await();
+        assertEquals("ABCDE", builder.toString());
+    }
+
+    public void testResolved() throws Exception {
+        assertTrue(Promise.resolved(null).isFulfilled());
+        assertTrue(Promise.rejected(null).isRejected());
+    }
+
+    public void testStatus() throws Exception {
+        Promise pending = new Promise();
+        assertTrue(pending.isPending());
+        assertFalse(pending.isFulfilled());
+        assertFalse(pending.isRejected());
+
+        Promise fulfilled = new Promise().resolve(null);
+        assertFalse(fulfilled.isPending());
+        assertTrue(fulfilled.isFulfilled());
+        assertFalse(fulfilled.isRejected());
+
+        Promise rejected = new Promise().reject(new Exception("Rejection"));
+        assertFalse(rejected.isPending());
+        assertFalse(rejected.isFulfilled());
+        assertTrue(rejected.isRejected());
+    }
+
+    public void testMultipleChain() throws Exception {
+        final StringBuilder builder = new StringBuilder();
+        final CountDownLatch latch = new CountDownLatch(1);
+
+        Handler handler = new Handler(Looper.getMainLooper());
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                Promise resolved = Promise.resolved(null);
+                resolved.then(new Callback() {
+                    @Override
+                    public void callback(Object result) throws Exception {
+                        builder.append("A");
+                    }
+                });
+                resolved.then(new Callback() {
+                    @Override
+                    public void callback(Object result) throws Exception {
+                        builder.append("B");
+                    }
+                });
+
+                final Promise<Character> pending = new Promise<>();
+                pending.then(new Callback<Character>() {
+                    @Override
+                    public void callback(Character result) throws Exception {
+                        builder.append("C");
+                    }
+                });
+                pending.then(new Callback<Character>() {
+                    @Override
+                    public void callback(Character result) throws Exception {
+                        builder.append(result);
+                    }
+                });
+                pending.resolve('D').then(new Callback<Character>() {
+                    @Override
+                    public void callback(Character result) throws Exception {
+                        latch.countDown();
+                    }
+                });
+            }
+        });
+
+        latch.await();
+
+        assertEquals("ABCD", builder.toString());
+    }
+
+    public void testResult() throws Exception {
+        Promise promise = new Promise().resolve("testResult");
+        assertEquals("testResult", promise.result());
+    }
 }
